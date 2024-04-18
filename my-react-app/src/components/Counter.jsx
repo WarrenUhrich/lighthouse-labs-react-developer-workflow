284e6a70652236fe1b70fb78bc0d8508a4e703ce
Warren Uhrich <hello@warren.codes>
2024-04-18 16:51:43 -0600

    Add Counter component

--- my-react-app/src/components/Counter.jsx
+++ my-react-app/src/components/Counter.jsx
@@ -1,11 +1,21 @@
-// import React from 'react';
+import React, { useState } from 'react';
 
-const Counter = () => {
+const Counter = (props) => {
+    const heading = props?.heading;
+
+    const [count, setCount] = useState(0);
+
+    const handleClick = () => {
+        setCount(prev => prev + 1);
+    };
 
     return (
-        <button>
-            Test
-        </button>
+        <section>
+            <h2>{heading}</h2>
+            <button onClick={handleClick}>
+                Clicked {count} times!
+            </button>
+        </section>
     );
 };
 
